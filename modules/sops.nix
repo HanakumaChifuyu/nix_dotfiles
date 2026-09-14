a718b428cf00ee04ff75dbe907edf1e411e2412e
HanakumaChifuyu <ytchencheng@gmail.com>
2026-09-14 10:50:49 +0800

    fix(sops): handle invalid TMPDIR during NixOS installation

--- modules/sops.nix
+++ modules/sops.nix
@@ -17,6 +17,8 @@ in
   sops.defaultSopsFile = ../secrets/keys.yaml;
   # This will automatically import SSH keys as age keys
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+  # All secrets use age; do not try importing an RSA host key for GPG.
+  sops.gnupg.sshKeyPaths = [ ];
   # This is using an age key that is expected to already be in the filesystem
   sops.age.keyFile = "/var/lib/sops-nix/key.txt";
   # This will generate a new key if the key specified above does not exist
@@ -25,19 +27,27 @@ in
   system.activationScripts.generateUserSopsAgeKey = {
     deps = [ "users" ];
     text = ''
+      # Isolate error handling and cleanup from the shared activation shell.
+      (
+      set -e
       host_key=/etc/ssh/ssh_host_ed25519_key
       user_age_key=${sopsUserAgeKeyFile}
       user_age_dir=$(${pkgs.coreutils}/bin/dirname "$user_age_key")
 
       if [ -f "$host_key" ]; then
         ${pkgs.coreutils}/bin/install -d -m 700 -o ${sopsUser} -g users "$user_age_dir"
-        tmp_key=$(${pkgs.coreutils}/bin/mktemp)
+        # nixos-install can pass a TMPDIR that does not exist inside the chroot.
+        # An absolute template avoids TMPDIR and allows an atomic final rename.
+        tmp_key=$(${pkgs.coreutils}/bin/mktemp "$user_age_dir/.keys.txt.XXXXXXXXXX")
+        trap '${pkgs.coreutils}/bin/rm -f -- "$tmp_key"' EXIT
         ${pkgs.ssh-to-age}/bin/ssh-to-age -private-key -i "$host_key" -o "$tmp_key"
-        ${pkgs.coreutils}/bin/install -m 600 -o ${sopsUser} -g users "$tmp_key" "$user_age_key"
-        ${pkgs.coreutils}/bin/rm -f "$tmp_key"
+        ${pkgs.coreutils}/bin/chmod 600 "$tmp_key"
+        ${pkgs.coreutils}/bin/chown ${sopsUser}:users "$tmp_key"
+        ${pkgs.coreutils}/bin/mv -f -- "$tmp_key" "$user_age_key"
       else
         echo "[sops] WARNING: $host_key not found; cannot derive user age key for Home Manager." >&2
       fi
+      )
     '';
   };
 
